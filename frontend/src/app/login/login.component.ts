45907b4bd2d5b764e4c58f131dc7847e3a4b1582
malak jackop <kikmalak26@gmail.com>
2025-08-12 16:06:03 +0300

    login validation

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -5,10 +5,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { routes } from '../app.routes';
 import { catchError, throttleTime, throwError } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule,RouterModule],
+  imports: [ReactiveFormsModule,RouterModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
@@ -18,6 +19,9 @@ export class LoginComponent {
   fb = inject(FormBuilder)
   httpClient = inject(HttpClient)
 
+    loginError: string | null = null;
+
+
   loginForm: FormGroup = this.fb.group({
     username: ['', Validators.required],
     password: ['', Validators.required]
@@ -25,29 +29,23 @@ export class LoginComponent {
 
   onSubmit(): void {
     console.log("login form", this.loginForm.value)
-    this.errorMessage = null;
+    this.loginError = null;
 
 
-  if (!this.loginForm.value.username || !this.loginForm.value.password) {
-    alert("Username or password is empty. Request not sent.");
-    return;
-  }
 
     if (this.loginForm.valid) {
       this.httpClient.post('http://localhost:8080/api/user/login', this.loginForm.value).pipe(
         catchError(error => {
-          this.errormessage = error.error?.Message || 'failed login';
-          return throwError(() => Error)};
+          this.loginError = error.error?.Message || 'failed login';
+          return throwError(() => Error)}
         )
       
     ).subscribe({
         next: (res) => {
           console.log('login successful :', res);
-          alert('login successful');
         },
         error: (err) => {
           console.error('Login failed:', err);
-                    alert('login failed');
         }
       })
     }
