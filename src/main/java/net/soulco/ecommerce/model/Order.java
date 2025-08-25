2c186df4a76c423f3546bf3a8deaa688b72f9673
meladhanna <eng.meladhanna10@yahoo.com>
2025-08-25 15:42:38 +0300

    Modify Order and Order Item create scripts and Models

--- src/main/java/net/soulco/ecommerce/model/Order.java
+++ src/main/java/net/soulco/ecommerce/model/Order.java
@@ -3,13 +3,14 @@ package net.soulco.ecommerce.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "order")
@@ -30,17 +32,20 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String uniqueIdentifier;
+    @Column(name = "identifier")
+    private String identifier;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Column(name = "total_amount")
     private BigDecimal totalAmount;
 
+    @Column(name = "order_date")
     private LocalDateTime orderDate;
 
-    public Object getUserId() {
-        return null;
-    }
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name="order_id")
+    private List<OrderItem> orderItems;
 }
--- src/main/java/net/soulco/ecommerce/model/OrderItem.java
+++ src/main/java/net/soulco/ecommerce/model/OrderItem.java
@@ -1,6 +1,7 @@
 package net.soulco.ecommerce.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,16 +27,17 @@ public class OrderItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "order_id")
-    private Order order;
+    @Column(name = "order_id")
+    private Long orderId;
 
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
 
+    @Column(name = "quantity")
     private Integer quantity;
 
+    @Column(name = "total_amount")
     private BigDecimal totalAmount;
 }
 
